e72595f69b0e03b63147b46bf7cda573be7d2ab0
thomas maestas <thomasm1.maestas@gmail.com>
2020-01-08 14:44:33 -0500

    fixed CORS

--- project1/src/main/java/servproject1/MasterServlet.java
+++ project1/src/main/java/servproject1/MasterServlet.java
@@ -18,16 +18,17 @@ public class MasterServlet extends HttpServlet {
 	@Override
 	protected void doGet(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
-		
+
 //		setAccessControlHeaders(response);
 //		PrintWriter writer = response.getWriter();
 //		writer.write("test response from myServlet");
 		HttpSession sess = request.getSession();
-		sess.setMaxInactiveInterval(3600); 
+		sess.setMaxInactiveInterval(3600);
 //		sess.setAttribute("m-owner", "{'username', 'tom'},{'ordernum','222'}"); 
 		System.out.println("MasterServlet: " + sess.getAttribute("m-owner"));
-		 
+
 		RequestHelper.process(request, response);
+		addCorsHeader(response);
 	}
 
 	@Override
@@ -36,6 +37,25 @@ public class MasterServlet extends HttpServlet {
 		doGet(request, response);
 	}
 
+	@Override
+	protected void doOptions(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		addCorsHeader(resp);
+	}
+
+	private void addCorsHeader(HttpServletResponse resp) {
+		// log.trace("adding headers");
+		resp.addHeader("Access-Control-Allow-Origin", "http://localhost:4200");
+		resp.addHeader("Vary", "Origin");
+		// if I don't care about getting my cookie, this will work
+		// response.addHeader("Access-Control-Allow-Origin", "*");
+		resp.addHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE, HEAD");
+		resp.addHeader("Access-Control-Allow-Headers", "X-PINGOTHER, Origin, X-Requested-With, Content-Type, Accept");
+		resp.addHeader("Access-Control-Allow-Credentials", "true");
+		resp.addHeader("Access-Control-Max-Age", "1728000");
+		resp.addHeader("Produces", "application/json");
+	}
+
+}
 //	// for Preflight
 //	@Override
 //	protected void doOptions(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
@@ -47,5 +67,3 @@ public class MasterServlet extends HttpServlet {
 //		resp.setHeader("Access-Control-Allow-Origin", "http://localhost:8090");
 //		resp.setHeader("Access-Control-Allow-Methods", "GET");
 //	}
-
-}
